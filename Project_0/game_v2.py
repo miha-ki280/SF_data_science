f9e2cda079d560e09e6b09b7e58654bd7e2df86e
miha-ki280 <olvet@inbox.ru>
2023-03-22 21:22:48 +0300

    19 attempts_game_v2.py

--- Project_0/game_v2.py
+++ Project_0/game_v2.py
@@ -14,7 +14,7 @@ def random_predict(number:int=1) -> int:
 
     while True:
         count += 1
-        predict_number = np.random.randint(1, 101) # предполагаемое число
+        predict_number = np.random.randint(1, 20) # предполагаемое число
         if number == predict_number:
             break # выход из цикла, если угадали
     return(count)
@@ -33,7 +33,7 @@ def score_game(random_predict) -> int:
 
     count_ls = [] # список для сохранения количества попыток
     np.random.seed(1) # фиксируем сид для воспроизводимости
-    random_array = np.random.randint(1, 101, size=(1000)) # загадали список чисел
+    random_array = np.random.randint(1, 20, size=(1000)) # загадали список чисел
 
     for number in random_array:
         count_ls.append(random_predict(number))
